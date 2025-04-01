495e6b4ae8ef290167a55b3d4983fc75f97183b8
Angelo Manrique <angelomanrique03@gmail.com>
2025-04-01 13:04:49 +0200

    [refactor] - Creo un contructor donde se inicializa el array que contendrá los productos con su cantidad

--- src/Examen.php
+++ src/Examen.php
@@ -7,15 +7,23 @@ use function PHPUnit\Framework\isEmpty;
 class Examen
 {
     // TODO: Examen Kata
+    private array $products;
+    public function __construct()
+    {
+        $this->products = [];
+    }
+
 
     public function getList(string $instruction): string
     {
         $instruction = explode(' ', $instruction);
 
         if (empty($instruction[2])) {
+            array_push($this->products, $instruction[1] . ' ' . 'x1');
             return $instruction[1] . ' ' . 'x1';
         }
 
+        array_push($this->products, $instruction[1] . ' ' . 'x' . $instruction[2]);
         return $instruction[1] . ' ' . 'x' . $instruction[2];
     }
 }
